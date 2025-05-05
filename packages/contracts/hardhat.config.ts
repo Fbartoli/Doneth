9a11581f0c01eb2b5338d5f36b1abd922b78ae6c
Florent Bartoli <florent.b@safe.global>
2025-05-05 21:08:37 +0200

    git ignore

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -2,6 +2,9 @@ import "@matterlabs/hardhat-zksync";
 import "@nomicfoundation/hardhat-toolbox";
 
 import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
@@ -12,10 +15,10 @@ const config: HardhatUserConfig = {
   },
 
   networks: {
-    lensTestnet: {
+    lens: {
       chainId: 232,
-      ethNetwork: "lens-mainnet",
-      url: "https://lens-mainnet.g.alchemy.com/v2/HtjVi-EgFf7Up_PGwYLEBKzwvxB-hw1f",
+      ethNetwork: "lens",
+      url: `https://lens-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_ID}`,
       verifyURL:
         "https://block-explorer-verify.testnet.lens.dev/contract_verification",
       zksync: true,
